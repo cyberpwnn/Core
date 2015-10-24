0a64f178bf3bfcad7ed5ec2042802bc33902c675
cyberpwn <danielmillst@gmail.com>
2015-10-24 10:32:30 -0400

    Player Loggers

--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
 @echo off
-java -Dcom.xtansia.ccemu.path="C:\Users\Daniel\Documents\GitHub\Core\src" -jar runtime/ccemuredux-launcher.jar
+java -Dcom.xtansia.ccemu.path="C:\Users\cyberpwn\Documents\GitHub\Core\src" -jar runtime/ccemuredux-launcher.jar
 echo Exiting
