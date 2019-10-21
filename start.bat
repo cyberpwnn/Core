fcc1e911c8a727ba39bb3b0b4bb850843e9b332f
Daniel Mills <danielmillst@gmail.com>
2019-10-21 17:11:42 -0400

    Update

--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
 @echo off
-java -Dcom.xtansia.ccemu.path="C:\Users\cyberpwn\Documents\GitHub\Core\src" -jar runtime/ccemuredux-launcher.jar
+java -Dcom.xtansia.ccemu.path="C:\Users\cyberpwn\Documents\development\workspace\Core\src" -jar runtime/ccemuredux-launcher.jar
 echo Exiting
